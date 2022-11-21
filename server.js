05ab8bd53ce06bef873b7d1f42836c400284718f
mktwohy <69822801+mktwohy@users.noreply.github.com>
2022-11-21 16:37:09 -0600

    add GET /incidents

--- server.js
+++ server.js
@@ -41,9 +41,16 @@ app.get('/neighborhoods', (req, res) => {
 
 // GET request handler for crime incidents
 app.get('/incidents', (req, res) => {
-    console.log(req.query); // query object (key-value pairs after the ? in the url)
+    let params = req.query
+    let query = `SELECT * FROM Incidents`
     
-    res.status(200).type('json').send({}); // <-- you will need to change this
+    databaseSelect(query)
+    .then((incidents) => {
+        res.status(200).type('json').send(incidents)
+    })
+    .catch((err) => {
+        res.status(404).type('text/plain').send(err)
+    })
 });
 
 // PUT request handler for new crime incident
